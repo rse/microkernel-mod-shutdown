d32e219eb196ece9ea1919299224f06e7e1a4d53
Ralf S. Engelschall <rse@engelschall.com>
2016-05-15 23:38:38 +0200

    place under version control

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# microkernel-mod-shutdown
-Microkernel extension module for providing process shutdown mechanisms
+
+microkernel-mod-shutdown
+=========================
+
+Microkernel extension module for providing process shutdown mechanisms.
+
+<p/>
+<img src="https://nodei.co/npm/microkernel-mod-shutdown.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/microkernel-mod-shutdown.png" alt=""/>
+
+About
+-----
+
+This is an extension module for the
+[Microkernel](http://github.com/rse/microkernel) server application
+environment, adding mechanisms for clean process shutdown. It
+provides explicit `fatal` and `shutdown` services and reacts on both
+process signals `SIGINT` (interrupt, CTRL-C), `SIGTERM` (terminate)
+and `SIGUSR2` (process manager control) and the process event
+`uncaughtException`.
+
+Usage
+-----
+
+```shell
+$ npm install microkernel
+$ npm install microkernel-mod-ctx microkernel-mod-logger
+$ npm install microkernel-mod-shutdown
+```
+
+License
+-------
+
+Copyright (c) 2016 Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ microkernel-mod-shutdown.js
@@ -0,0 +1,103 @@
+/*
+**  Microkernel -- Microkernel for Server Applications
+**  Copyright (c) 2015-2016 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+export default class Module {
+    get module () {
+        return {
+            name:  "microkernel-mod-shutdown",
+            tag:   "SHUTDOWN",
+            group: "BOOT",
+            after: [ "CTX", "LOGGER" ]
+        }
+    }
+    boot (kernel) {
+        /*  perform application shutdown  */
+        let shutdown = (reason, cb) => {
+            kernel.state("dead").then(() => {
+                /*  perform final callback  */
+                kernel.sv("log", "shutdown", "info", `shutdown process (reason: ${reason})`)
+                kernel.sv("log", "shutdown", "info", `#### TERMINATING PROCESS ####`)
+                cb.call(null, reason)
+            }, (err) => {
+                kernel.sv("log", "shutdown", "error", `shutdown process FAILED: ${err} (${err.stack})`)
+            })
+        }
+
+        /*  handle fatal application error  */
+        kernel.register("fatal", (error) => {
+            kernel.sv("log", "shutdown", "error", "FATAL ERROR: " + error)
+            shutdown("FATAL", () => process.exit(1))
+        })
+
+        /*  standard termination  */
+        kernel.register("shutdown", (info) => {
+            kernel.sv("log", "shutdown", "info", "terminating process: " + info)
+            shutdown("SHUTDOWN", () => process.exit(0))
+        })
+
+        /*  handle signals  */
+        let signals = [ "SIGUSR2", "SIGINT", "SIGTERM" ]
+        let seen = {}
+        signals.forEach((signal) => {
+            let handler = () => {
+                /*  react only once
+                    NOTICE: we cannot use process.once(), because we regularily
+                    (for unknown reasons) become the same signal delivered twice during
+                    the shutdown processing, so let us ignore it here explicitly. */
+                if (seen[signal])
+                    return
+                seen[signal] = true
+
+                /*  terminate the process by removing the signal handler (ourself)
+                    and re-delivering exactly the same signal once again (which is
+                    important at least for nodemon, as it expects we get terminated by
+                    exactly the signal SIGUSR2 he sent us)  */
+                let terminate = (sig, hand) => {
+                    process.removeListener(sig, hand)
+                    process.kill(process.pid, sig)
+                }
+                if (kernel.rs("ctx:procmode") === "worker") {
+                    /*  deferred delivery (to give master a chance to react first)  */
+                    setTimeout(() => {
+                        kernel.sv("log", "shutdown", "info", `#### received ${signal} signal (DEFERRED) ####`)
+                        shutdown(signal, () => terminate(signal, handler))
+                    }, 4 * 1000)
+                }
+                else {
+                    /*  immediate delivery  */
+                    kernel.sv("log", "shutdown", "info", `#### received ${signal} signal ####`)
+                    shutdown(signal, () => terminate(signal, handler))
+                }
+            }
+            process.on(signal, handler)
+        })
+
+        /*  handle exceptions  */
+        process.on("uncaughtException", (error) => {
+            kernel.sv("log", "shutdown", "error", `#### received uncaught exception: ${error.message} ####\n${error.stack}`)
+            shutdown("EXCEPTION", () => process.exit(1))
+        })
+    }
+}
+
